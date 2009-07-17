89f9f73acf60f3bad03ac6b77e940d728cbe5e85
Rodrigo Tassinari de Oliveira <rodrigo@pittlandia.net>
2009-07-17 20:37:32 -0300

    Realça div votado.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,11 +1,12 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
-function prepare_and_submit_vote_form(winner, loser) {
+function prepare_and_submit_vote_form(winner, loser, key) {
+  var div = (key == '1' ? '#voteA' : '#voteB')
+  $(div).effect("highlight");
   $('#votes form#new_vote #vote_winner_id').attr('value', winner)
   $('#votes form#new_vote #vote_loser_id').attr('value', loser)
   $('#votes form#new_vote').submit();
   $('body > div.container_16').fadeTo('fast', 0.33);
   $('#loading').show();
 }
-
